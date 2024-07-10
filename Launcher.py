4c73cfff3185c10c794baf2f3013ea62bc16b32f
CBKSTL <157271678+CBKSTL@users.noreply.github.com>
2024-07-10 10:36:04 +0630

    Update Launcher.py

--- Launcher.py
+++ Launcher.py
@@ -47,6 +47,6 @@ async def main():
     for i in range(len(cogs)):
         await cogs[i].setup(client)
 
-token = 'token'
+token = 'MTI2MDI1NzM5ODgwMzkyMjk2NQ.G9m1sa.ywZM1F-5PdC52dNctWCRaoSv6X-yeZtxc1WZxk'
 asyncio.run(main())
 client.run(token)
